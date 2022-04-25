c2672f71ee89b0c41a0848825c9332fdf7b2d0ab
Witalis Domitrz <witekdomitrz@gmail.com>
2022-04-25 21:24:14 +0200

    Input

--- src/brandes.cu
+++ src/brandes.cu
@@ -1,4 +1,40 @@
+#include <algorithm>
+#include <cstdint>
+#include <fstream>
 #include <iostream>
-using std::cout;
+#include <string>
+#include <vector>
 
-int main() { cout << "asd\n"; }
+#include "compact_graph_representation.hpp"
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        std::cerr << "Two arguments are required\n"
+                     "./brandes input-file output-file\n";
+        exit(1);
+    }
+    const std::string input_file_name(argv[1]), out_file_name(argv[2]);
+    std::ifstream input_file(input_file_name);
+    std::vector<std::pair<node_id, node_id>> edges;
+
+    node_id a = (unsigned long long)123;
+    node_id node_1, node_2;
+    node_id max_node_id = 0;
+
+    while (input_file >> node_1 >> node_2) {
+        edges.push_back(std::make_pair(node_1, node_2));
+    }
+
+    Compact_graph_representation graph(edges);
+
+    node_id n = graph.size();
+    const node_id *compact_graph = graph.get_compact_graph();
+    const node_id *starting_positions = graph.get_starting_positions_of_nodes();
+
+    for (int i = 0; i < n; i++) {
+        for (int j = starting_positions[i]; j < starting_positions[i + 1];
+             j++) {
+            std::cout << i << " " << compact_graph[j] << "\n";
+        }
+    }
+}
---
+++ src/compact_graph_representation.hpp
@@ -0,0 +1,60 @@
+#include <cstdint>
+#include <vector>
+
+using node_id = int32_t;
+class Compact_graph_representation {
+    // With a guardian at the end
+    std::vector<node_id> starting_positions_of_nodes;
+    std::vector<node_id> compact_graph;
+    node_id number_of_nodes;
+
+   private:
+    void add_directed_edge(node_id node_1, node_id node_2,
+                           std::vector<node_id>& considered_edges_from_node) {
+        compact_graph[starting_positions_of_nodes[node_1] +
+                      considered_edges_from_node[node_1]] = node_2;
+        considered_edges_from_node[node_1]++;
+    }
+
+   public:
+    Compact_graph_representation(
+        std::vector<std::pair<node_id, node_id>>& edges) {
+        node_id max_node_id = 0;
+        for (const auto& [node_1, node_2] : edges)
+            if (node_2 > max_node_id) max_node_id = node_2;
+
+        number_of_nodes = max_node_id + 1;
+        starting_positions_of_nodes.resize(number_of_nodes + 1, 0);
+        compact_graph.resize(2 * edges.size());
+
+        for (const auto& [node_1, node_2] : edges) {
+            starting_positions_of_nodes[node_1]++;
+            starting_positions_of_nodes[node_2]++;
+        }
+
+        // be optimized, but not worth it -- not a bootle neck at all
+        for (std::size_t i = 1; i < starting_positions_of_nodes.size(); i++) {
+            starting_positions_of_nodes[i] +=
+                starting_positions_of_nodes[i - 1];
+        }
+        for (std::size_t i = starting_positions_of_nodes.size() - 1; i > 0;
+             i--) {
+            starting_positions_of_nodes[i] = starting_positions_of_nodes[i - 1];
+        }
+        starting_positions_of_nodes[0] = 0;
+
+        std::vector<node_id> considered_edges_from_node(number_of_nodes, 0);
+        for (const auto& [node_1, node_2] : edges) {
+            add_directed_edge(node_1, node_2, considered_edges_from_node);
+            add_directed_edge(node_2, node_1, considered_edges_from_node);
+        }
+    }
+
+    const node_id* get_starting_positions_of_nodes() {
+        return starting_positions_of_nodes.data();
+    }
+
+    const node_id* get_compact_graph() { return compact_graph.data(); }
+
+    node_id size() { return number_of_nodes; }
+};
