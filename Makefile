df39011462e8630c31e1a7ecfb1593067f4cd3f2
Witalis Domitrz <witekdomitrz@gmail.com>
2022-04-30 23:09:40 +0200

    array version in progress

--- Makefile
+++ Makefile
@@ -1,12 +1,22 @@
-.PHONY: all clean distclean testsclean
+.PHONY: all clean distclean test-clean test
 
 all: brandes
 
 COMPILER_OPTIONS := -Wextra -Wall -O3
+G++_COMPILER_OPTIONS := -Wall -Wextra -Wpedantic -O3
 CUDA_COMPILER_OPTIONS := $(foreach option, $(COMPILER_OPTIONS), --compiler-options $(option))
 
-brandes: $(wildcard src/*.cu) $(wildcard src/*.h)
-	nvcc $(CUDA_COMPILER_OPTIONS) -o brandes src/*.cu
+brandes: src/brandes.cu src/brandes.hpp
+	nvcc $(CUDA_COMPILER_OPTIONS) -o brandes src/brandes.cu
+
+brandes-seq: src/brandes.cpp src/brandes.hpp
+	g++ $(G++_COMPILER_OPTIONS) -o brandes src/brandes.cpp src/brandes-seq.cpp
+
+brandes-seq-vector: src/brandes.cpp src/brandes.hpp
+	g++ $(G++_COMPILER_OPTIONS) -o brandes src/brandes.cpp src/brandes-seq-vector.cpp
+
+brandes-seq-array: src/brandes.cpp src/brandes.hpp
+	g++ $(G++_COMPILER_OPTIONS) -o brandes src/brandes.cpp src/brandes-seq-array.cpp
 
 clean:
 	rm -f src/*.o
@@ -14,7 +24,7 @@ clean:
 distclean: clean
 	rm -f brandes
 
-testsclean:
+test-clean:
 	rm -f res-*.txt errors.txt *.csv *.log
 
 test:
--- src/brandes-seq-vector.cpp
+++ src/brandes-seq-vector.cpp
@@ -1,7 +1,5 @@
 #include <cstdint>
 #include <iostream>
-#include <queue>
-#include <stack>
 #include <vector>
 
 #include "brandes.hpp"
@@ -9,23 +7,22 @@
 void brandes(const int32_t n, const int32_t starting_positions[],
              const int32_t compact_graph[], double CB[]) {
     for (int32_t s = 0; s < n; s++) {
-        std::stack<int32_t> S;
+        std::vector<int32_t> S;
         std::vector<std::vector<int32_t>> P(n);
         std::vector<int32_t> sigma(n, 0);
         sigma[s] = 1;
         std::vector<int32_t> d(n, -1);
         d[s] = 0;
-        std::queue<int32_t> Q;
-        Q.push(s);
-        while (!Q.empty()) {
-            int32_t v = Q.front();
-            Q.pop();
-            S.push(v);
+        std::vector<int32_t> Q;
+        Q.push_back(s);
+        for (size_t Q_pos = 0; Q_pos < Q.size(); Q_pos++) {
+            int32_t v = Q[Q_pos];
+            S.push_back(v);
             int32_t end = starting_positions[v + 1];
             for (int32_t i = starting_positions[v]; i < end; i++) {
                 int32_t w = compact_graph[i];
                 if (d[w] < 0) {
-                    Q.push(w);
+                    Q.push_back(w);
                     d[w] = d[v] + 1;
                 }
                 if (d[w] == d[v] + 1) {
@@ -35,9 +32,8 @@ void brandes(const int32_t n, const int32_t starting_positions[],
             }
         }
         std::vector<double> delta(n, 0.0);
-        while (!S.empty()) {
-            int32_t w = S.top();
-            S.pop();
+        for (size_t i = S.size(); i > 0; i--) {
+            int32_t w = S[i - 1];
 
             for (size_t i = 0; i < P[w].size(); i++) {
                 int32_t v = P[w][i];
