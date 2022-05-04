2727ba0bdde5e42e989760acef06a0518d88123f
Witalis Domitrz <witekdomitrz@gmail.com>
2022-05-04 02:57:31 +0200

    4 min version

--- test_all.sh
+++ test_all.sh
@@ -1,4 +1,4 @@
-for t in brandes-seq brandes-seq-vector brandes-seq-array brandes-par-vert brandes-par-edge brandes-par-vert-queue brandes-par-vert-comp brandes-par-edge-comp brandes-par-vert-queue-comp; do
+for t in brandes-seq brandes-seq-vector brandes-seq-array brandes-par-vert brandes-par-edge brandes-par-vert-queue brandes-par-vert-comp brandes-par-edge-comp brandes-par-vert-queue-comp brandes-par-vert-virt; do
     echo $t
     rm -f ./brandes
     make $t
